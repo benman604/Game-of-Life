c0c656d107e2cc760485504e618368d99259e5d2
Benjamin Man <benjamin.man1001@gmail.com>
2021-06-04 23:23:43 -0700

    add menu options

--- life.py
+++ life.py
@@ -1,15 +1,17 @@
 from tkinter import *
-import tkinter as tk
 import math
-import time
-
+import webbrowser
+import random
 size = 600
 speed = 250
-playing = True
-top = tk.Tk()
-top.title = "Placeholder"
-box = tk.PhotoImage("iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=")
+currentcount = 0
+playing = False
+top = Tk()
+top.geometry("460x500")
+box = PhotoImage("iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=")
 cells = []
+activeColor = "gray"
+inactiveColor = "white"
 
 class Grid(Frame):  # Create grid of cells
     def __init__(self, root):
@@ -17,8 +19,8 @@ class Grid(Frame):  # Create grid of cells
         for row in range(round(math.sqrt(size))):
             cells.append([])
             for column in range(round(math.sqrt(size))):
-                item = tk.Button(self, text="", image=box, height=15, width=15, borderwidth=0, bg='white', activebackground='white')
-                item.grid(row=row, column=column)
+                item = Button(self, text="", image=box, height=15, width=15, borderwidth=0, bg='white', activebackground='white')
+                item.grid(row=row, column=column, padx=1, pady=1)
                 cells[len(cells)-1].append(item)
                 item.configure(command=lambda x=[row, column]: self.gridItemClick(x[0], x[1]))
         nextIttr()
@@ -26,35 +28,52 @@ class Grid(Frame):  # Create grid of cells
     def gridItemClick(self, x, y):  # Toggle button state on clicked
         print(x, y)
         if isAlive(cells[x][y]):
-            cells[x][y].configure(bg="white")
-            cells[x][y].configure(activebackground="white")
+            cells[x][y].configure(bg=inactiveColor)
+            cells[x][y].configure(activebackground=inactiveColor)
         else:
-            cells[x][y].configure(bg="gray")
-            cells[x][y].configure(activebackground="gray")
+            cells[x][y].configure(bg=activeColor)
+            cells[x][y].configure(activebackground=activeColor)
 
 
 class Controls(Frame):
     def __init__(self, root):
+        global currentcount
         Frame.__init__(self, root)
-        play = tk.Button(self, text="Play", borderwidth=0, bg="white", fg="black", command=self.playClick).grid(row=0, column=0)
-        pause = tk.Button(self, text="Pause", borderwidth=0, bg="white", fg="black", command=self.pauseClick).grid(row=0, column=1)
-        reset = tk.Button(self, text="Reset", borderwidth=0, bg="white", fg="black", command=self.resetClick).grid(row=0, column=2)
+
+        self.state = StringVar()
+        self.state.set("Play")
+        self.play = Button(self, textvariable=self.state, borderwidth=0, bg=inactiveColor, fg="black", command=self.playClick).grid(row=0, column=0)
+
+        self.count = StringVar()
+        self.count.set("0")
+        self.counter = Button(self, textvariable=self.count, borderwidth=0, fg="black").grid(row=0, column=1)
+
+        Button(self, text="Clear", borderwidth=0, bg=inactiveColor, fg="black", command=self.resetClick).grid(row=0, column=2)
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_columnconfigure(1, weight=1)
 
     def playClick(self):
         global playing
-        playing = True
-        nextIttr()
-    def pauseClick(self):
-        global playing
-        playing = False
+        global play
+        playing = not playing
+        if playing:
+            nextIttr()
+            self.state.set("Pause")
+        else:
+            self.state.set("Play")
+
     def resetClick(self):
         global playing
+        global currentcount
+        currentcount = 0
         for row in cells:
             for item in row:
-                item.configure(bg="white")
-                item.configure(activebackground="white")
+                item.configure(bg=inactiveColor)
+                item.configure(activebackground=inactiveColor)
         playing = False
+        self.state.set("Play")
 
+controls = Controls(top)
 
 def nextIttr():  # Called once per iteration
     deathIntents = []
@@ -70,17 +89,22 @@ def nextIttr():  # Called once per iteration
                     birthIntents.append(item)
 
     for item in deathIntents:
-        item.configure(bg="white")
-        item.configure(activebackground="white")
+        item.configure(bg=inactiveColor)
+        item.configure(activebackground=inactiveColor)
     for item in birthIntents:
-        item.configure(bg="gray")
-        item.configure(activebackground="gray")
+        item.configure(bg=activeColor)
+        item.configure(activebackground=activeColor)
+
+    global currentcount
+    global controls
+    currentcount += 1
+    controls.count.set(str(currentcount))
 
     if playing:
         top.after(speed, nextIttr)
 
 def isAlive(cell):  # Returns current state of cell
-    if cell.cget("bg") == "white":
+    if cell.cget("bg") == inactiveColor:
         return False
     else:
         return True
@@ -108,7 +132,45 @@ def checkNeighbors(x, y):  # Returns number of living cells surrounding x, y
 
 grid = Grid(top)
 grid.pack(side="top", fill="both")
-controls = Controls(top)
 controls.pack(side="bottom", fill="x", expand=True)
-tk.call('wm', 'Iconphoto', top._w, tk.PhotoImage('icon.png'))
+
+def setspeed(newspeed):
+    global speed
+    speed = newspeed
+def showAbout():
+    webbrowser.open("https://github.com/benman604/Game-of-Life", new=1)
+def fillRandom():
+    global playing
+    global currentcount
+    playing = False
+    for row in cells:
+        for cell in row:
+            r = random.randint(0, 1)
+            if r == 0:
+                cell.configure(bg=inactiveColor)
+                cell.configure(activebackground=inactiveColor)
+            else:
+                cell.configure(bg=activeColor)
+                cell.configure(activebackground=activeColor)
+                controls.state.set("Play")
+                currentcount = 0
+
+menu = Menu(top)
+
+speedmenu = Menu(menu, tearoff=0)
+speedmenu.add_command(label="0.25", command=lambda: setspeed(1000))
+speedmenu.add_command(label="0.5", command=lambda: setspeed(750))
+speedmenu.add_command(label="1", command=lambda: setspeed(500))
+speedmenu.add_command(label="1.25", command=lambda: setspeed(250))
+speedmenu.add_command(label="1.75", command=lambda: setspeed(100))
+speedmenu.add_command(label="2", command=lambda: setspeed(50))
+menu.add_cascade(label="Speed", menu=speedmenu)
+menu.add_command(label="Fill Random", command=fillRandom)
+menu.add_command(label="About", command=showAbout)
+
+icon = PhotoImage(file="icon.png")
+top.iconphoto(False, icon)
+
+top.config(menu=menu)
+top.title('Game of Life')
 top.mainloop()
